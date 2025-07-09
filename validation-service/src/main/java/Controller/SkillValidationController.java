1350892a74533ba981790da634ce1ff794662cf3
Oumayma Saidi <oumaymasaidi908@gmail.com>
2025-07-09 11:08:18 +0100

    Obtenir les validations par submissionId et subSkillId

--- validation-service/src/main/java/Controller/SkillValidationController.java
+++ validation-service/src/main/java/Controller/SkillValidationController.java
@@ -1,4 +1,42 @@
 package Controller;
 
+import com.enaa.validationservice.model.SkillValidation;
+import com.enaa.validationservice.service.SkillValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/skillvalidations")
+@CrossOrigin(origins = "*") // optionnel pour autoriser les appels dlfront
 public class SkillValidationController {
+
+    @Autowired
+    private SkillValidationService service;
+
+    // add validation
+    @PostMapping
+    public SkillValidation ajouterValidation(@RequestBody SkillValidation validation) {
+        return service.ajouter(validation);
+    }
+
+    // Obtenir toutes les validations
+    @GetMapping
+    public List<SkillValidation> getAll() {
+        return service.getAll();
+    }
+
+    // Obtenir les validations par submissionId
+    @GetMapping("/submission/{id}")
+    public List<SkillValidation> getBySubmission(@PathVariable Long id) {
+        return service.getBySubmissionId(id);
+    }
+
+    // Obtenir les validations par subSkillId
+    @GetMapping("/subskill/{id}")
+    public List<SkillValidation> getBySubSkill(@PathVariable Long id) {
+        return service.getBySubSkillId(id);
+    }
 }
+
--- validation-service/src/main/java/service/SkillValidationService.java
+++ validation-service/src/main/java/service/SkillValidationService.java
@@ -1,11 +1,9 @@
-package service;
+package com.enaa.validationservice.service;
 
-
-import Model.SkillValidation;
+import com.enaa.validationservice.model.SkillValidation;
+import com.enaa.validationservice.repository.SkillValidationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import repository.SkillValidationRepository;
-
 import java.util.List;
 
 @Service
@@ -16,17 +14,21 @@ public class SkillValidationService {
 
     public SkillValidation ajouter(SkillValidation validation) {
         return repository.save(validation);
+
     }
 
     public List<SkillValidation> getBySubmissionId(Long id) {
         return repository.findBySubmissionId(id);
+
     }
 
     public List<SkillValidation> getBySubSkillId(Long id) {
         return repository.findBySubSkillId(id);
+
     }
 
     public List<SkillValidation> getAll() {
         return repository.findAll();
+
     }
 }
