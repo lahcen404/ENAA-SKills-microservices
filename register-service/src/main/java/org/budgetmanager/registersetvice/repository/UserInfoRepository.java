eee8989fd41e29f0058d9793192aae9714714200
Oumayma Saidi <oumaymasaidi908@gmail.com>
2025-07-14 16:32:55 +0100

    UserInfoRepository

--- register-service/src/main/java/org/budgetmanager/registersetvice/repository/UserInfoRepository.java
+++ register-service/src/main/java/org/budgetmanager/registersetvice/repository/UserInfoRepository.java
@@ -1,4 +1,12 @@
 package org.budgetmanager.registersetvice.repository;
 
-public interface UserInfoRepository {
+import org.budgetmanager.registersetvice.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
 }
