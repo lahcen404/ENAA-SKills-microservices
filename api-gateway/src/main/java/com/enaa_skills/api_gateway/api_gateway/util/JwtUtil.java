fabbc902e9b9bd0cb362076f385900f9ef21b80c
lahcen404 <lahcen.maskour2003@gmail.com>
2025-07-15 14:40:25 +0100

    create JwtUtil file for validate Token

---
+++ api-gateway/src/main/java/com/enaa_skills/api_gateway/api_gateway/util/JwtUtil.java
@@ -0,0 +1,24 @@
+package com.enaa_skills.api_gateway.api_gateway.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+
+@Component
+public class JwtUtil {
+
+    public final static String SECRET = "5367566859703373367639792F423F452848284D6251655468576D5A71347437";
+
+    public void validateToken(final String token){
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
