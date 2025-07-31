0b1bb3c0fbe9010bf76fd045a0cd93b48f5db96c
krrsalmane <karrounsalmane@gmail.com>
2025-07-31 10:22:05 +0100

    create the components

---
+++ enaa-skills-frontend/src/app/features/briefs/brief-assignment/brief-assignment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brief-assignment',
+  standalone: true,
+  imports: [],
+  templateUrl: './brief-assignment.component.html',
+  styleUrl: './brief-assignment.component.css'
+})
+export class BriefAssignmentComponent {
+
+}
