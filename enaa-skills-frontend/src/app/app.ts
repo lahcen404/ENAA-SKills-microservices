33cb19ef7d592651241780602695a945eb0067aa
krrsalmane <karrounsalmane@gmail.com>
2025-07-31 15:32:03 +0100

    fix an error in the components

--- enaa-skills-frontend/src/app/app.ts
+++ enaa-skills-frontend/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BriefAssignmentComponent } from './features/briefs/brief-assignment/brief-assignment.component';
+import { BriefFormComponent } from './features/briefs/brief-form/brief-form.component';
+import { BriefListComponent } from './features/briefs/brief-list/brief-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BriefAssignmentComponent,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
