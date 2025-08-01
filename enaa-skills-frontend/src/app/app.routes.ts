81bb3a43aacdf3f269d92106001fd3184ef4fac8
suilahi <suilahizakaria@gmail.com>
2025-08-01 15:05:22 +0100

    ajouter route

--- enaa-skills-frontend/src/app/app.routes.ts
+++ enaa-skills-frontend/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { Routes } from '@angular/router';
 import {CompetenceForm} from './features/competences/competence-form/competence-form';
 
 import {CompetenceList} from './features/competences/competence-list/competence-list';
+import {Login} from './features/auth/login/login';
+import {Register} from './features/auth/register/register';
 
 export const routes: Routes = [
 
   {path:'add-skill',component:CompetenceForm},
 
-  {path:'skills',component:CompetenceList}
+  {path:'skills',component:CompetenceList},
+  {path:'login',component:Login},
+  {path:'register',component:Register}
+
 ];
