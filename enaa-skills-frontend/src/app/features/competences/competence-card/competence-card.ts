087e1076b2dc3c2f6509dd41cc0b92260db4490d
lahcen404 <lahcen.maskour2003@gmail.com>
2025-08-02 15:51:05 +0100

    Delete skill done ! !

--- enaa-skills-frontend/src/app/features/competences/competence-card/competence-card.ts
+++ enaa-skills-frontend/src/app/features/competences/competence-card/competence-card.ts
@@ -1,4 +1,4 @@
-import { Component , Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Skill} from '../../../core/modules/skill';
 import {SkillService} from '../../../core/services/skill/skill';
 import {NgForOf} from '@angular/common';
@@ -15,11 +15,22 @@ import {Router} from '@angular/router';
 export class CompetenceCard {
 
   @Input() skill!:Skill;
+  @Output() deleted=new EventEmitter<number>();
+
 
   constructor(private skillService: SkillService,
               private router: Router){}
 
   onEdit(skill: Skill){
     this.router.navigate(['edit-skill',skill.id]);
+  }
+
+  onDelete(id:number){
+    if (confirm('are you sure you want to delete this skill ??')){
+      this.skillService.deleteSkill(id).subscribe(()=>{
+        alert("Skill deleted successfully !!");
+        this.deleted.emit(id);
+      })
+    }
   }
 }
